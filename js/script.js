8dc883bf3dc296198d821243be7310994e7483ca
Donato-Carola <don94@live.it>
2023-11-21 15:09:52 +0100

    prima prova nel compiere l'esercizio

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
+
+//! Creo la variabile const button per richiamarla
+
+const button = document.querySelector('button')
+const mainGrid = document.querySelector('main')
+
+
+
+//! Una volta richiamata le assococio l'evento click, cioè quando clicca il button crea i quadrati
+button.addEventListener('click', function(){
+    for(let i=1; i<=100; i++){
+     const boxSquare = document.createElement('span')
+     mainGrid.appendChild(boxSquare)
+    }
+    
+    
+})
